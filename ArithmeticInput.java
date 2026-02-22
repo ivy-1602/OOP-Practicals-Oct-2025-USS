f7eeeffaae4b03a1e756dc30539c4703ddf8d5ae
Ivy <uma2007salunke@gmail.com>
2026-02-22 06:43:41 +0530

    Create ArithmeticInput.java

---
+++ ArithmeticInput.java
@@ -0,0 +1,22 @@
+/**
+ * Arithmetic Operations Calculator
+ * Performs all basic operations: +, -, *, /, %
+ */
+import java.util.Scanner;
+
+public class ArithmeticInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+        sc.close();
+    }
+}
